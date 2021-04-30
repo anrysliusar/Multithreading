d03fe39794e1c0d7f99c97a121a21154defecc36
Andrew <assliusarenko@gmail.com>
2021-04-30 23:50:50 +0300

    add main method that checks reading and printing

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.concurrent.atomic.AtomicBoolean;
 
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+        LineStorage lineStorage = new LineStorage();
+        BufferedReader bufferedReader = new BufferedReader(new FileReader("src/lines.txt"));
+
+        AtomicBoolean isLinePrinted = new AtomicBoolean(false);
+
+        ContentPrinter contentPrinter = new ContentPrinter(lineStorage, isLinePrinted);
+        ContentReader contentReader = new ContentReader(lineStorage, bufferedReader, isLinePrinted);
+
+        Thread reader = new Thread(contentReader);
+        Thread printer = new Thread(contentPrinter);
+
+        reader.start();
+        printer.start();
     }
 }
