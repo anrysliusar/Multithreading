46d5aae1e78d9adebff40168b694a276504e7f8e
Andrew <assliusarenko@gmail.com>
2021-04-30 23:48:51 +0300

    add ContentReader and ContentPrinter (to read and print data from file(through LineStorage))

--- src/ContentPrinter.java
+++ src/ContentPrinter.java
@@ -1,2 +1,34 @@
-public class ContentPrinter {
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class ContentPrinter implements Runnable{
+    private LineStorage lineStorage;
+    private AtomicBoolean isLinePrinted;
+
+    public ContentPrinter(LineStorage lineStorage, AtomicBoolean isLinePrinted) {
+        this.lineStorage = lineStorage;
+        this.isLinePrinted = isLinePrinted;
+    }
+
+    @Override
+    public void run() {
+        synchronized (lineStorage){
+            while (!lineStorage.isFinished().get()){
+                System.out.println("2");
+                while (isLinePrinted.get()){
+                    try{
+                        System.out.println("3");
+
+                        lineStorage.wait();
+                    }catch (Exception e){
+                        e.printStackTrace();
+                    }
+                }
+                System.out.println(lineStorage.getLine());
+                isLinePrinted.set(true);
+                lineStorage.notifyAll();
+            }
+        }
+        System.out.println("Printer finished");
+
+    }
 }
--- src/ContentReader.java
+++ src/ContentReader.java
@@ -1,2 +1,42 @@
-public class ContentReader {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class ContentReader implements Runnable{
+    private LineStorage lineStorage;
+    private BufferedReader bufferedReader;
+    private AtomicBoolean isLinePrinted;
+
+
+    public ContentReader(LineStorage lineStorage, BufferedReader bufferedReader, AtomicBoolean isLinePrinted) {
+        this.lineStorage = lineStorage;
+        this.bufferedReader = bufferedReader;
+        this.isLinePrinted = isLinePrinted;
+    }
+
+    @Override
+    public void run() {
+        String line;
+        synchronized (lineStorage){
+            try {
+
+                while ((line = bufferedReader.readLine()) != null){
+                    lineStorage.setLine(line);
+                    lineStorage.notifyAll();
+                    System.out.println("1");
+                    try {
+                        lineStorage.wait();
+                    }catch (Exception e){
+                        e.printStackTrace();
+                    }
+                }
+
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+        lineStorage.isFinished().set(true);
+        isLinePrinted.set(false);
+        lineStorage.notifyAll();
+    }
 }
