2a95baaeb3f872c53e25f7f3b212295ffa0a2f97
Andrew <assliusarenko@gmail.com>
2021-04-30 23:45:51 +0300

    add LineStorage

--- src/LineStorage.java
+++ src/LineStorage.java
@@ -1,2 +1,19 @@
+import java.util.concurrent.atomic.AtomicBoolean;
+
 public class LineStorage {
+    private String line = "";
+    private AtomicBoolean finished = new AtomicBoolean(false);
+
+    public String getLine() {
+        return line;
+    }
+
+    public void setLine(String line) {
+        this.line = line;
+    }
+
+    public AtomicBoolean isFinished() {
+        return finished;
+    }
+
 }
